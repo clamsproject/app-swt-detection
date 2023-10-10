3ae8d7bfbbdf37b034704ef46d1c58692f651e03
lbaumgarten <baumgartenleora@gmail.com>
2023-10-10 01:17:17 -0400

    initial (buggy) training

--- modeling/train.py
+++ modeling/train.py
@@ -1,18 +1,14 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torchvision.transforms.functional as TF
+from torchvision.io import read_image
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from pathlib import Path
-from skimage import io
 import argparse
-import matplotlib.pyplot as plt
-import numpy as np
+from d2l import torch as d2l
+import time
+from tempfile import TemporaryDirectory
 
-
-label_keys = ["S","I","N","Y","C","B","W","L","O","M","E","K","G","T","F","R"]
 
 class FrameDetectionDataset(Dataset):
     def __init__(self, table, images):
@@ -26,28 +22,27 @@ class FrameDetectionDataset(Dataset):
         if torch.is_tensor(index):
             index = index.tolist()
         image_name = self.table.iloc[index]['filename']
-        image = io.imread(self.images[image_name])
-        return (TF.to_tensor(image), self.table.iloc[index]['type label'])
+        image = read_image(str(self.images[image_name])).float()
+        label = get_label(self.table.iloc[index]['type label'])
+        return (image, label)
     
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 4)
+def get_label(label):
+    slate = ["S"]
+    chyron = ["I","N","Y"]
+    credit = ["C"]
+    if label in slate:
+        return 0
+    elif label in chyron:
+        return 1
+    elif label in credit:
+        return 2
+    else:
+        return 3
 
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) 
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+def get_net():
+    num_classes = 10
+    net = d2l.resnet18(num_classes, 3)
+    return net
 
 def build_dataset(directory):
     p = Path(directory)
@@ -62,36 +57,61 @@ def build_dataset(directory):
     return FrameDetectionDataset(table, image_names)
 
 def train(dataset):
-    slate = ["S"]
-    chyron = ["I","N","Y"]
-    credit = ["C"]
-    not_interested = ["B","W","L","O","M","E","K","G","T","F","R"]
-    classes = ('slate', 'chyron', 'credit', 'not-interested')
-    trainloader = DataLoader(dataset, shuffle=True)
-    net = Net()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    for epoch in range(2):  # loop over the dataset multiple times
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
+    train_loader = DataLoader(dataset, batch_size=4, shuffle=True)      
+    train_size = len(dataset) 
+    class_names =  ['slate', 'chyron', 'credit', 'not-interested']                
+    loss = nn.CrossEntropyLoss(reduction="none")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    train_model(get_net(), train_loader, train_size, loss, device)
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
+def train_model(model, train_loader, train_size, criterion, device, num_epochs=25):
+    since = time.time()
 
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = Path(tempdir) / 'best_model_params.pt'
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
-                                                               
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
+
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+            print('-' * 10)
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in train_loader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                with torch.set_grad_enabled(True):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+                        loss.sum().backward()
+
+                running_loss += loss.sum().item() * inputs.size(0)
+                print(preds)
+                print(labels.data)
+                running_corrects += torch.sum(preds == labels.data)
+
+                epoch_loss = running_loss / train_size
+                epoch_acc = running_corrects.double() / train_size
+
+                print(f'Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                
+
+            print()
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Best val Acc: {best_acc:4f}')
+
+        model.load_state_dict(torch.load(best_model_params_path))
+    return model
+
+                                                           
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("root_directory", help="root directory containing the images to train on")
