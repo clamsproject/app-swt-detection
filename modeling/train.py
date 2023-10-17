3d2823273810f901697d377ee437aafdbe27d443
lbaumgarten <baumgartenleora@gmail.com>
2023-10-16 21:17:57 -0400

    video-level evaluation

--- modeling/train.py
+++ modeling/train.py
@@ -13,29 +13,28 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class SWTDataset(Dataset):
-    def __init__(self, in_dir, feature_model, allow_guids=[], block_guids=[]):
-        self.feat_dim = 4096 if feature_model == 'vgg16' else 2048  # for now, there are only two models
-        self.labels = []
-        self.vectors = []
-        if not allow_guids:
-            # TODO (krim @ 10/10/23): implement whitelisting
-            for j in Path(in_dir).glob('*.json'):
-                guid = j.with_suffix("").name
-                if guid not in block_guids:
-                    feature_vecs = np.load(Path(in_dir) / f"{guid}.{feature_model}.npy")
-                    labels = json.load(open(Path(in_dir) / f"{guid}.json"))
-                    for i, vec in enumerate(feature_vecs):
-                        l = int_encode(labels['frames'][i]['label'])
-                        self.labels.append(int_encode(labels['frames'][i]['label']))
-                        self.vectors.append(torch.from_numpy(vec))
+    def __init__(self, feat_dim, labels, vectors, allow_guids=[]):
+        self.feat_dim = feat_dim 
+        self.labels = labels
+        self.vectors = vectors
+        # TODO (krim @ 10/10/23): implement whitelisting
 
     def __len__(self):
         return len(self.labels)
     
     def __getitem__(self, i):
         return self.vectors[i], self.labels[i]
-    
-    
+
+
+def get_guids(dir, block_guids=[]):
+    guids = []
+    for j in Path(dir).glob('*.json'):
+        guid = j.with_suffix("").name
+        if guid not in block_guids:
+            guids.append(guid)
+    return guids
+
+
 def int_encode(label):
     slate = ["S"]
     chyron = ["I", "N", "Y"]
@@ -59,17 +58,71 @@ def get_net(dim=4096):
     )
 
 
-def train(dataset):
-    train, valid = torch.utils.data.random_split(dataset, [0.8, 0.2])
-    train_loader = DataLoader(train, batch_size=40, shuffle=True)
-    valid_loader = DataLoader(valid, batch_size=len(valid), shuffle=True)
-    print(len(train), len(valid))
-    loss = nn.CrossEntropyLoss(reduction="none")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    train_model(get_net(dataset.feat_dim), train_loader, valid_loader, loss, device)
+def split_dataset(indir, valid_guid, feature_model):
+    feat_dim = 4096 if feature_model == 'vgg16' else 2048  # for now, there are only two models
+    train_vectors = []
+    train_labels = []
+    valid_vectors = []
+    valid_labels = []
+    for j in Path(indir).glob('*.json'):
+        guid = j.with_suffix("").name
+        feature_vecs = np.load(Path(indir) / f"{guid}.{feature_model}.npy")
+        labels = json.load(open(Path(indir) / f"{guid}.json"))
+        if guid == valid_guid:
+            for i, vec in enumerate(feature_vecs):
+                l = int_encode(labels['frames'][i]['label'])
+                valid_labels.append(int_encode(labels['frames'][i]['label']))
+                valid_vectors.append(torch.from_numpy(vec))   
+        else:
+             for i, vec in enumerate(feature_vecs):
+                l = int_encode(labels['frames'][i]['label'])
+                train_labels.append(int_encode(labels['frames'][i]['label']))
+                train_vectors.append(torch.from_numpy(vec))
+    train = SWTDataset(feat_dim, train_labels, train_vectors)
+    valid = SWTDataset(feat_dim, valid_labels, valid_vectors)
+    return train, valid
 
 
-def train_model(model, train_loader, valid_loader, loss_fn, device, num_epochs=25):
+def train(indir, k_fold, featuremodel):
+    guids = get_guids(indir)
+    p_scores = []
+    r_scores = []
+    f_scores = []
+    for i in range(k_fold):
+        valid_guid = guids[i]
+        train, valid = split_dataset(indir, valid_guid, featuremodel)
+        train_loader = DataLoader(train, batch_size=40, shuffle=True)
+        valid_loader = DataLoader(valid, batch_size=len(valid), shuffle=True)
+        print(len(train), len(valid))
+        loss = nn.CrossEntropyLoss(reduction="none")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model, p, r, f = train_model(get_net(train.feat_dim), train_loader, valid_loader, loss, device, valid_guid)
+        p_scores.append(p)
+        r_scores.append(r)
+        f_scores.append(f)
+    print_scores(p_scores, r_scores, f_scores)
+    
+
+def print_scores(p_scores, r_scores, f_scores):
+    max = f_scores.index(max(f_scores))
+    min = f_scores.index(min(f_scores))
+    print("Highest p/r/f is")
+    print(f'\tprecision = {p_scores[max]}')
+    print(f'\trecall = {r_scores[max]}')
+    print(f'\tf-1 = {f_scores[max]}')
+    print("Lowest p/r/f is")
+    print(f'\tprecision = {p_scores[max]}')
+    print(f'\trecall = {r_scores[max]}')
+    print(f'\tf-1 = {f_scores[max]}')
+    print("Mean p/r/f is")
+    print(f'\tprecision = {sum(p_scores)/len(p_scores)}')
+    print(f'\trecall = {sum(r_scores)/len(r_scores)}')
+    print(f'\tf-1 = {sum(f_scores)/len(f_scores)}')
+
+
+def train_model(model, train_loader, valid_loader, loss_fn, device, valid_guid, num_epochs=25):
+    print(f'Using the guid {valid_guid} to evaluate')
+
     since = time.time()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
@@ -111,9 +164,6 @@ def train_model(model, train_loader, valid_loader, loss_fn, device, num_epochs=2
             m = metrics.confusion_matrix(preds, vlabels, 'multiclass', num_classes=4)
 
             print(f'Loss: {epoch_loss:.4f} after {num_epoch+1} epochs')
-            print(f'Precision: {p:.4f} after {num_epoch+1} epochs')
-            print(f'Recall: {r:.4f} after {num_epoch+1} epochs')
-            print(f'F-1: {f:.4f} after {num_epoch+1} epochs')
             print(m)
             print("slate, chyron, credit, none")
             
@@ -121,13 +171,14 @@ def train_model(model, train_loader, valid_loader, loss_fn, device, num_epochs=2
         print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
 
         model.load_state_dict(torch.load(best_model_params_path))
-    return model
+        print()
+    return model, p, r, f
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("indir", help="root directory containing the vectors and labels to train on")
     parser.add_argument("featuremodel", help="feature vectors to use for training", choices=['vgg16', 'resnet50'], default='vgg16')
+    parser.add_argument("k_fold", help="the number of distinct dev sets to evaluate on", default=10)
     args = parser.parse_args()
-    dataset = SWTDataset(args.indir, args.featuremodel)
-    train(dataset)
+    train(args.indir, args.k_fold, args.featuremodel)
