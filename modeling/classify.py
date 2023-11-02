0fe7fc1dde0280eb92153013681ebfe5ea72da88
Marc Verhagen <marc@cs.brandeis.edu>
2023-11-02 19:53:14 -0400

    Added code to create time frames from the series of time point scores

--- modeling/classify.py
+++ modeling/classify.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+from operator import itemgetter
 
 import torch
 import numpy as np
@@ -16,7 +17,7 @@ import backbones
 # For now just using the model from the first fold of a test (and also assuming
 # that it has the labels as listed in config/default.yml).
 BEST_MODEL = 'results-test/20231026-135541.kfold_000.pt'
-#BEST_MODEL = 'results-test/20231026-164841.kfold_000.pt'
+BEST_MODEL = 'results-test/20231026-164841.kfold_000.pt'
 
 # Assuming for now that the model's feature extractor uses the VGG16 model.
 MODEL_TYPE = 'vgg16'
@@ -28,12 +29,22 @@ LABEL_MAPPINGS = {0: 'slate', 1: 'chyron', 2: 'credit', 3: 'other'}
 # Milliseconds between frames.
 STEP_SIZE = 1000
 
+# Minimum average score for a timeframe. We require at least one frame score
+# higher than 1.
+MINIMUM_SCORE = 1.01
+
 # For debugging, set to True if you want to save the frames that were extracted.
 SAFE_FRAMES = False
 
 # Set to True if you want the script to be more verbose.
 DRIBBLE = False
 
+# Defining the bins for the labels.
+SCORE_MAPPING = ((0.01, 0), (0.25, 1), (0.50, 2), (0.75, 3), (1.01, 4))
+
+
+# Getting the non-other labels.
+LABELS = {label for label in sorted(LABEL_MAPPINGS.values()) if label != 'other'}
 
 # Loading the model and featurizer.
 model = train.get_net(4096, 4, 3, 0.2)
@@ -49,11 +60,11 @@ def process_video(mp4_file: str, step: int = 1000):
     all_predictions = []
     for n, image in get_frames(mp4_file, step):
         img = Image.fromarray(image[:,:,::-1])
-        features = process_frame(img, featurizer)
+        features = extract_features(img, featurizer)
         prediction = model(features)
-        prediction = softmax(prediction.detach().numpy())
+        prediction = Prediction(n, prediction)
         if DRIBBLE:
-            print(f'{n:05d}', prediction)
+            print(f'{n:07d}', prediction)
         all_predictions.append(prediction)
         if SAFE_FRAMES:
             cv2.imwrite(f"frames/frame-{n:06d}.jpg", image)
@@ -72,12 +83,14 @@ def get_frames(mp4_file: str, step: int = 1000):
         yield n, image
 
 
-def process_frame(frame_vec: np.ndarray, model: torch.nn.Sequential) -> torch.Tensor:
+def extract_features(frame_vec: np.ndarray, model: torch.nn.Sequential) -> torch.Tensor:
     """Extract the features of a single frame. Based on, but not identical to, the
     process_frame() method of the FeatureExtractor class in data_ingestion.py."""
     frame_vec = model.preprocess(frame_vec)
     frame_vec = frame_vec.unsqueeze(0)
     if torch.cuda.is_available():
+        if DRIBBLE:
+            print('CUDA is available')
         frame_vec = frame_vec.to('cuda')
         model.model.to('cuda')
     with torch.no_grad():
@@ -90,8 +103,11 @@ def softmax(x):
 
 
 def save_predictions(predictions: list, filename: str):
+    json_obj = []
+    for prediction in predictions:
+        json_obj.append(prediction.as_json())
     with open(filename, 'w') as fh:
-        json.dump(predictions, fh)
+        json.dump(json_obj, fh)
         print(f'Saved output to {filename}')
 
 
@@ -101,13 +117,101 @@ def load_predictions(filename: str) -> list:
         return predictions
 
 
+def enrich_predictions(predictions: list):
+    """For each prediction, add a nominal score for each label. The scores go from
+    0 through 4. For example if the raw probability score for the slate is in the
+    0.5-0.75 range than ('slate', 3) will be added."""
+    for prediction in predictions:
+        binned_scores = compute_labels(prediction[1])
+        prediction[1].append(binned_scores)
+
+
 def print_predictions(predictions):
-    print('\n     bars   slate  creds  other   label')
-    for n, (p1, p2, p3, p4) in enumerate(predictions):
-        scores = [p1, p2, p3, p4]
-        label = LABEL_MAPPINGS[scores.index(max(scores))]
-        print(f'{n:03}  {p1:.4f} {p2:.4f} {p3:.4f} {p4:.4f}  {label}')
-    print(f'\nTOTAL NUMBER: {len(predictions)}\n')
+    print('\n     slate  chyron creds  other')
+    for prediction in predictions:
+        milliseconds = prediction[0]
+        p1, p2, p3, p4 = prediction[1][:4]
+        binned_scores = prediction[1][4]
+        labels = ' '.join([f'{label}-{score}' for label, score in binned_scores])
+        print(f'{milliseconds:6}  {p1:.4f} {p2:.4f} {p3:.4f} {p4:.4f}  {labels}')
+    print(f'\nTOTAL PREDICTIONS: {len(predictions)}\n')
+
+
+def compute_labels(scores: list):
+    return (
+        ('slate', scale(scores[0])),
+        ('chyron', scale(scores[1])),
+        ('credit', scale(scores[2])))
+
+
+def scale(score):
+    """Put the score on a scale from 0 through 4, where 0 means the score is less
+    than 0.01 and 1 though 4 are quartiles for score bins 0.01-0.25, 0.25-0.50,
+    0.50-0.75 and 0.75-1.00."""
+    for score_in, score_out in SCORE_MAPPING:
+        if score < score_in:
+            return score_out
+
+
+def collect_timeframes(predictions: list) -> dict:
+    """Find sequences of frames for all labels where the score is not 0."""
+    timeframes = { label: [] for label in LABELS}
+    open_frames = { label: [] for label in LABELS}
+    for prediction in predictions:
+        timepoint = prediction[0]
+        bins = prediction[1][4]
+        for label, score in bins:
+            if score == 0:
+                if open_frames[label]:
+                    timeframes[label].append(open_frames[label])
+                open_frames[label] = []
+            elif score >= 1:
+                open_frames[label].append((timepoint, score, label))
+    for label, score in bins:
+        if open_frames[label]:
+            timeframes[label].append(open_frames[label])
+    return timeframes
+
+def compress_timeframes(timeframes: dict):
+    """Compresses all timeframes from [(t_1, score_1), ...  (t_n, score_n)] into the
+    shorter representation (t_1, t_n, average_score)."""
+    for label in LABELS:
+        frames = timeframes[label]
+        for i in range(len(frames)):
+            start = frames[i][0][0]
+            end = frames[i][-1][0]
+            score = sum([e[1] for e in frames[i]]) / len(frames[i])
+            frames[i] = (start, end, score)
+
+def filter_timeframes(timeframes: dict):
+    """Filter out all timeframes with an average score below the threshold defined
+    in MINIMUM_SCORE."""
+    for label in LABELS:
+        timeframes[label] = [tf for tf in timeframes[label] if tf[2] > MINIMUM_SCORE]
+
+def remove_overlapping_timeframes(timeframes: dict) -> list:
+    all_frames = []
+    for label in timeframes:
+        for frame in timeframes[label]:
+            all_frames.append(frame + (label,))
+    all_frames = list(sorted(all_frames, key=itemgetter(2), reverse=True))
+    outlawed_timepoints = set()
+    final_frames = []
+    for frame in all_frames:
+        if is_included(frame, outlawed_timepoints):
+            continue
+        final_frames.append(frame)
+        start, end, _, _ = frame
+        for p in range(start, end + STEP_SIZE, STEP_SIZE):
+            outlawed_timepoints.add(p)
+    return all_frames
+
+def is_included(frame, outlawed_timepoints):
+    start, end, _, _ = frame
+    for i in range(start, end + STEP_SIZE, STEP_SIZE):
+        if i in outlawed_timepoints:
+            return True
+    return False
 
 
 def experiment():
@@ -130,16 +234,49 @@ def experiment():
         print(outputs)
 
 
+class Prediction:
+
+    """Class to store a prediction from the model. It is meant to simplify the rest
+    of the code a bit and manage some of the intricacies of the data structures that
+    are involved. One thing it does is to run softmax over the scores in the tensor.
+
+    timepoint  -  the location of the frame, in milliseconds
+    tensor     -  the tensor that results from running the model on the features
+    data       -  the tensor simplified into a simple list with scores for each label
+
+    """
+
+    def __init__(self, timepoint: int, prediction: torch.Tensor):
+        self.timepoint = timepoint
+        self.tensor = prediction
+        self.data = softmax(self.tensor.detach().numpy())[0].tolist()
+
+    def __str__(self):
+        return f'<Prediction {self.timepoint} {self.data}>'
+
+    def as_json(self):
+        return [self.timepoint, self.data]
+
+
 if __name__ == '__main__':
 
     create_frame_predictions = False
-    create_timeframes = True
+    create_timeframes = False
 
     if create_frame_predictions:
         predictions = process_video('data/cpb-aacip-690722078b2-shrunk.mp4', step=STEP_SIZE)
-        predictions = [prediction[0].tolist() for prediction in predictions]
         save_predictions(predictions, 'predictions.json')
 
     if create_timeframes:
         predictions = load_predictions('predictions.json')
-        print_predictions(predictions)
+        enrich_predictions(predictions)
+        #print_predictions(predictions)
+        timeframes = collect_timeframes(predictions)
+        compress_timeframes(timeframes)
+        filter_timeframes(timeframes)
+        #for label in timeframes:
+        #    print(label, timeframes[label])
+        timeframes = remove_overlapping_timeframes(timeframes)
+        print(timeframes)
+
+
