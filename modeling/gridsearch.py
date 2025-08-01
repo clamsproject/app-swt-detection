0df5589ad23823ccac990d9d785fe5eb8053d58b
Keigh Rim <keigh.rim@gmail.com>
2025-08-01 08:26:40 -0400

    updated batch information from new data drops and training plan

--- modeling/gridsearch.py
+++ modeling/gridsearch.py
@@ -1,9 +1,8 @@
 import itertools
-import math
 
 import modeling.backbones
 import modeling.config.bins
-from modeling.config.batches import unintersting_guids, aapb_collaboration_27_b, aapb_collaboration_27_c, aapb_collaboration_27_e, guids_with_confirmed_slates_images
+from modeling.config.batches import excluded_guids
 
 
 ## TP classifier training grid search
@@ -23,22 +22,9 @@ pos_abs_th_end = {10}
 pos_vec_coeff = {0, 0.5}  # when 0, positional encoding is not enabled
 
 # to see effect of training data size
-block_guids_train = [
-    
-    # aapb_collaboration_27_a + aapb_collaboration_27_b + aapb_collaboration_27_c + aapb_collaboration_27_e + guids_with_confirmed_slates_images,  # exclude all training data
-    # First, count of GUIDs in annotation batches: a + b + c + e + f(?) = 20 + 21 + 20 + 60 + 1118 = 1239 videos 
-    # excluding `d` batch (reserved for evaluation),  including 1 uninsteresting video in `27-b` batch 
-    # (`d` batch has a single GUID, and never "vectorized" to `npy` files, hence not explicitly specified here)
-    # unintersting_guids,  # this used to be the "full" size (hashed as 0001@xxx in the result visualization until v7.5)
-    unintersting_guids + guids_with_confirmed_slates_images,  # OLD v7.1rc training set (hashed as 1119@xxx), now this is the "full" training set that we decided to hold "confirmed-slate" set out for evaluation of KIE task (see https://github.com/clamsproject/aapb-collaboration/issues/27#issuecomment-2841848595)
-    # note that the "uninstresting" video is never used in all training sets
-    # also note that when `split_size` is very large, no k-fold is performed, and the `pbd` set is used for fixed validation
-]
+block_guids_train = [excluded_guids]
 # since we now do validation on a fixed set, this parameter has no effect, keeping it for historical reasons
-block_guids_valid = [
-    unintersting_guids + aapb_collaboration_27_b + aapb_collaboration_27_e + guids_with_confirmed_slates_images,  # block all loosely-annotated videos and the challenging images
-    #  unintersting_guids,  # effectively no block except
-]
+block_guids_valid = []
 
 # "prebin" configurations. 
 # NOTE that postbin is not a part of the CV model, so is not handled here
